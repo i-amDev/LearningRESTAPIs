17b5a73f6f8da76f7043fe866cb1c5f1d490a025
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-12-19 13:25:36 +0530

    - Refactored.

--- src/main/java/com/learning/LearningRESTAPIs/entity/StudentEntity.java
+++ src/main/java/com/learning/LearningRESTAPIs/entity/StudentEntity.java
@@ -15,7 +15,8 @@ public class StudentEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private String email;
 
+    private String name;
+
+    private String email;
 }
