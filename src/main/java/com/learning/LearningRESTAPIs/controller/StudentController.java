aa69735d992294a75543a295388d9c0ded5cd550
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-12-19 13:24:38 +0530

    - Refactored. - Removed unused import.

--- src/main/java/com/learning/LearningRESTAPIs/controller/StudentController.java
+++ src/main/java/com/learning/LearningRESTAPIs/controller/StudentController.java
@@ -5,7 +5,6 @@ import com.learning.LearningRESTAPIs.dto.StudentDto;
 import com.learning.LearningRESTAPIs.service.StudentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
